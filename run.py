5550c19b1d5706e7b81b21dfee2fe294f0a23343
Claudia Schoeman <claudiaschoeman1@gmail.com>
2024-09-07 14:09:44 +0000

    Add main_menu function

--- run.py
+++ run.py
@@ -1,3 +1,18 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def main_menu():
+    """
+    Run welcome screen to main menu,
+    user enters name and selects quiz difficulty
+    """
+    print("Risky Quizness")
+
+    # User to enter their name
+    player_name = input("What is your name: ").strip().title()
+    print("Risky Quizness")
+    print(player_name)
+    
+
+main_menu()
